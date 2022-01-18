10df4feb3d3fa8a84c24d4935fce0b97528afa44
olasunkanmi <olasunkanmiibalogun@gmail.com>
2022-01-18 06:15:28 +0100

    mobile home page completed

--- src/Home.js
+++ src/Home.js
@@ -5,14 +5,13 @@ const Home = () => {
         <body>
          <main>
          <section className='home'>
-         <p>So, you want to travel to <span>space</span>
-         </p>
-
-        <p>let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of the edge of it. Well sit back and relax because we'll give you a truly out of this world experience!
+         <div className='desc'>
+         <p>So, you want to travel to</p> <p>space</p>
+         <p>let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of the edge of it. Well sit back and relax because we'll give you a truly out of this world experience!
           </p>
+        </div>
         
-
-         <div className='explore'>
+        <div className='explore'>
          <h1>Explore</h1>
          </div>
          </section>
