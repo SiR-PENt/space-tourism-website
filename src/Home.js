cacc9a4d8aa236d9ea6a34bb9b5962eb4e436f03
olasunkanmi <olasunkanmiibalogun@gmail.com>
2022-01-18 17:22:41 +0100

    mobile homepage with fonts

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,26 @@
 import React from 'react'
 
 const Home = () => {
-    return (
-        <body>
+    return (    
          <main>
          <section className='home'>
+          <aside className='aside'>
+          <nav className='nav'>
+          </nav>
+          </aside>
+          
          <div className='desc'>
          <p>So, you want to travel to</p> <p>space</p>
-         <p>let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of the edge of it. Well sit back and relax because we'll give you a truly out of this world experience!
+         <p>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of the edge of it. Well sit back and relax because we'll give you a truly out of this world experience!
           </p>
         </div>
         
         <div className='explore'>
-         <h1>Explore</h1>
+         <p>Explore</p>
          </div>
          </section>
          </main>
-         </body> 
+        
         //  display grid on the body
     )
 }
